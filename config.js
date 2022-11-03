9a7dae1a45d5bcacb7763f10f65c4858f685d5e2
ALEXANDER <114868672+ALEXANERALAM@users.noreply.github.com>
2022-11-03 12:37:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919810138006'] //ur owner number
+global.ownername = "➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 " //ur owner name
+global.ytname = "YT: MOG GAMING" //ur yt chanel name
+global.socialm = "GitHub: ➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 " //ur github or insta name
+global.location = "INDIA, DELHI, MANGOLPURI" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.owner = ['919810138006'] //ur number
+global.ownernomer = "919810138006" //ur number
+global.ownertag = '919810138006' //ur tag number
+global.premium = ['919810138006'] //ur premium number
+global.botname = '➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 ' //ur bot name
+global.ownername = "➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 "
+global.linkz = "https://instagram.com/its_a13x_papa?igshid=YmMyMTA2M2Y=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/its_a13x_papa?igshid=YmMyMTA2M2Y= " //ur website to be displayed
+global.botscript = 'https://instagram.com/its_a13x_papa?igshid=YmMyMTA2M2Y=' //script link
+global.themeemoji = "🥵" //ur theme emoji
+global.packname = "➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 " //ur sticker watermark packname
+global.author = "➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 " //ur sticker watermark author
+global.wm = "➣⃟⃟🦇ꪶ͢𝜜𝑳𝜩𝑿𝜜𝑁𝐷𝜩𝑹ꫂ ⃟⃟⃟⃟🐈‍⬛🖤 " //ur watermark
 
 // Other
 global.sessionName = 'session'
